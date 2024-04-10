6650b07e6a3cabe6fa7d385bc079cf55df11f1a4
vr2lousir <68815618+vr2lousir@users.noreply.github.com>
2024-04-10 14:43:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,27 @@
+let IRstate = 0
+function IRreading () {
+    if (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine) && CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
+        IRstate = 11
+    } else if (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine) && CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.BlackLine)) {
+        IRstate = 10
+    } else if (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.BlackLine) && CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
+        IRstate = 1
+    } else {
+        IRstate = 0
+    }
+}
+function wheelAction (state: number) {
+    if (state == 0) {
+        CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.Forward, 30)
+    } else if (state == 1) {
+        CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnLeft, 30)
+    } else if (state == 10) {
+        CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, 30)
+    } else {
+        CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.Backward, 30)
+    }
+}
+basic.forever(function () {
+    IRreading()
+    wheelAction(IRstate)
+})
